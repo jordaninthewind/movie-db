65f95ccb40348d898310c6164d3db06956088563
Jordan Kline <jordan.kline@gmail.com>
2018-12-07 10:53:11 -0800

    added filter bar for total results show and filter toggle

--- src/containers/SearchResults/SearchResults.js
+++ src/containers/SearchResults/SearchResults.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import MovieTile from '../../components/MovieTile/MovieTile';
+import DisplayTiles from '../../components/DisplayTiles/DisplayTiles';
+import ResultsFilter from '../../components/ResultsFilter/ResultsFilter';
 import './SearchResults.css';
 
 class SearchResults extends Component {
@@ -10,24 +11,27 @@ class SearchResults extends Component {
   }
 
   render() {
-    if ( this.props.loading ) {
-      return <div>Loading...</div>;
-    } else if ( this.props.movies.length !== 0 ) {
-      return (
-        <ul id="searchResults">
-          { this.props.movies.map(movie => <MovieTile movie={movie} key={movie.id} />) }
-        </ul>
-      )
-    } else {
-      return <div id="noResults">No films found</div>;
-    }
+    return (
+      <>
+        <ResultsFilter 
+          total={this.props.total}
+        />
+        <DisplayTiles 
+          movies={this.props.movies} 
+          loading={this.props.loading}
+          input={this.props.input}
+        />
+      </>
+    )
   }
 }
 
 const mapStateToProps = state => {
   return {
     movies: state.moviesReducer.movies,
-    loading: state.moviesReducer.loading
+    loading: state.moviesReducer.loading,
+    input: state.moviesReducer.input,
+    total: state.moviesReducer.totalResults
   }
 }
 
