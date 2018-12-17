6c2ad5ae27bca738dbd89812f552bed8d4bbc212
Jordan Kline <jordan.kline@gmail.com>
2018-12-16 16:20:28 -0800

    added link to imdb in title on show page for convenience and curiosity

--- src/components/ShowDetails/ShowDetails.js
+++ src/components/ShowDetails/ShowDetails.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import './ShowDetails.css'
+import React from 'react';
+import './ShowDetails.css';
 
 const ShowDetails = props => {
   return (
     <div className="showDetails">
-      <div>{props.movie.title}<span id="rating">{Number.parseFloat(props.movie.vote_average).toFixed(1)}</span></div>
-      <span>{new Date(props.movie.release_date).getFullYear()}</span><span>{props.movie.runtime} minutes</span>
+      <div>
+        <a href={`http://www.imdb.com/title/` + props.movie.imdb_id}
+          target="_blank" >
+          {props.movie.title}
+        </a>
+        <span id="rating">
+          {Number.parseFloat(props.movie.vote_average).toFixed(1)}
+        </span>
+      </div>
+      <span>{new Date(props.movie.release_date).getFullYear()}</span>
+      <span>{props.movie.runtime} minutes</span>
       <p>{props.movie.overview}</p>
     </div>
-  )
-}
+  );
+};
 
-export default ShowDetails
+export default ShowDetails;
